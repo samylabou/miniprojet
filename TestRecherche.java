cdcc5b264e43c4688d9a5f6923a52618186d6f12
Meugi <kandrobox@gmail.com>
2015-12-01 21:42:17 +0100

    Code TestRecherche fait par KANDE_BELAID

---
+++ TestRecherche.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class TestRecherche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int tab[]={1,2,3,4,5,6};
+		char tab1[] = {'a','b'};	
+		
+			
+		/*	Recherche r = new Recherche();
+				
+			System.out.println(r.rechercheDicho(tab1,6,7));
+			
+			//System.out.println(r.rechercheDichoChar(tab1,3,'m'));*/
+	}
+
+}
