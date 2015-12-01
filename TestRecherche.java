5a5add025da483c869e45490f2dddc016b49ac2a
Meugi <kandrobox@gmail.com>
2015-12-01 22:35:38 +0100

    Code TestRecherche fait par KANDE_BELAID modifier

--- TestRecherche.java
+++ TestRecherche.java
@@ -6,12 +6,12 @@ public class TestRecherche {
 		// TODO Auto-generated method stub
 
 		int tab[]={1,2,3,4,5,6};
-		char tab1[] = {'a','b'};	
+			
 		
 			
-		/*	Recherche r = new Recherche();
+	         	Recherche r = new Recherche();
 				
-			System.out.println(r.rechercheDicho(tab1,6,7));
+			System.out.println(r.rechercheDicho(tab1,6,3));
 			
 			//System.out.println(r.rechercheDichoChar(tab1,3,'m'));*/
 	}
