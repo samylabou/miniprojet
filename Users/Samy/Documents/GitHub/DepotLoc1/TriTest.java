8e99dfce167312303144a85efe6c352617c82294
samylabou <samy-labou@laposte.net>
2015-12-05 16:39:46 +0100

    On branch master Initial commit Changes to be committed: 	new file:   ../DepotLoc1/Tri.java 	new file:   ../DepotLoc1/TriTest.java

---
+++ Users/Samy/Documents/GitHub/DepotLoc1/TriTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.univ.Tri;
+
+import static org.junit.Assert.*;
+
+public class TriTest {
+    @Test
+    public void canConstructTri() {
+    	int[] iArr = {5,4,15,19,10,8,7};
+        Tri tri = new Tri( iArr[7] );
+        assertEquals(  , person.getName());
+    }
+}
