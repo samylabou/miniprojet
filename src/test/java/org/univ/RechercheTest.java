3a511ffb49d207cab88897c887850a18676a8472
Meugi <kandrobox@gmail.com>
2015-12-07 00:49:43 +0100

    NouvelleVersion

---
+++ src/test/java/org/univ/RechercheTest.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import org.junit.Test;
+import org.univ.Recherche;
+import static org.junit.Assert.*;
+public class RechercheTest {
+      @Test 
+	public  void ArriveARechercherUnELement(){
+		// TODO Auto-generated method stub
+
+		int tab[]={1,2,3,4,5,6};
+			
+		
+			
+	         	Recherche r = new Recherche();
+				
+			System.out.println(r.rechercheDicho(tab,6,3));
+			
+			//System.out.println(r.rechercheDichoChar(tab1,3,'m'));*/
+	}
+
+}
