a1dc9d62c1e114cc1b09dffb93c9f3a2a0e0cd0f
samylabou <samy-labou@laposte.net>
2015-12-07 20:45:28 +0100

    Create RechercheTest.java

---
+++ RechercheTest.java
@@ -0,0 +1,19 @@
+//package org.univ;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RechercheTest {
+
+
+    @Test
+    public void testMethodeRecherche() {
+      String[] tableauEntreeNonTrie = {"05","04","15","19","10","08","07"};
+		Tri tri = new Tri();
+	 String[] tab = tri.triTab(tableauEntreeNonTrie);
+	  String entree = "08";
+        Recherche recherche = new Recherche();
+        assertEquals( entree, recherche.rechTab(tab, entree));
+ 	}
+}
